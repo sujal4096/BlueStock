386cd68121c62151aa4bae0f763a019b0087ffbd
Shiva-Doulagar <shivadoulager@gmail.com>
2025-02-25 21:53:35 +0530

    completed signup page

---
+++ sign_up/script.js
@@ -0,0 +1,14 @@
+document.getElementById("togglePassword").addEventListener("click", function () {
+    let passwordInput = document.getElementById("exampleInputPassword1");
+    let icon = this;
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        icon.classList.remove("bi-eye-slash");
+        icon.classList.add("bi-eye");
+    } else {
+        passwordInput.type = "password";
+        icon.classList.remove("bi-eye");
+        icon.classList.add("bi-eye-slash");
+    }
+});
